24bd21866ef307d2b86c7cc5544c5ed33a71e898
nat0t <nataniel1987@gmail.com>
2021-08-19 14:41:09 +0300

    Added task2.

---
+++ lesson1/task2.py
@@ -0,0 +1,35 @@
+"""
+Дополнить следующую функцию недостающим кодом:
+
+def print_directory_contents(sPath):
+\"""
+Функция принимает имя каталога и распечатывает его содержимое в виде «путь и имя файла», а также любые другие файлы во
+вложенных каталогах.
+\"""
+
+Эта функция подобна os.walk. Использовать функцию os.walk нельзя. Данная задача показывает ваше умение работать с
+вложенными структурами.
+"""
+
+
+import os
+
+
+def print_directory_contents(path: str) -> None:
+    """
+    Функция принимает имя каталога и распечатывает его содержимое в виде «путь и имя файла», а также любые другие файлы
+    во вложенных каталогах.
+    """
+
+    if os.path.isdir(path):
+        content = os.listdir(path)
+        for name in content:
+            filepath = os.path.join(path, name)
+            if not os.path.isdir(filepath):
+                print(filepath)
+            else:
+                print_directory_contents(filepath)
+
+
+if __name__ == '__main__':
+    print_directory_contents('/home/master/Development/interview_prepare/')
